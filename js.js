479ba5858cd68dd9ba62702b29d85064b6fc1c25
Pik2Pika <ja2002t@gmx.com>
2022-09-07 18:47:56 -0400

    complete exercise

---
+++ js.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('#container');
+const para = document.createElement('p');
+const header = document.createElement('h3');
+const div = document.createElement('div');
+const paraChild = document.createElement('p');
+const headerChild= document.createElement('h1');
+
+para.textContent = 'Hey i\'m red!';
+para.setAttribute('style', 'color: red;' );
+header.textContent = "i'm a blue h3!";
+header.setAttribute('style','color:blue;');
+div.setAttribute('style', 'border-color:black; background-color:pink;');
+headerChild.textContent = "i'm in a div";
+paraChild.textContent = "ME TOO!";
+
+
+div.append(headerChild, paraChild);
+container.append(para, header, div);
+//container.appendChild(para);
+//container.appendChild(header);
+
+//container.appendChild(div);
+
